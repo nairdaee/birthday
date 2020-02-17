ca86ac34482d4599f4acb0b1cb8e002ce2eea277
Adrian Etenyi <etadriano2@gmail.com>
2020-02-17 23:25:47 +0300

    make untrackable code trackable

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-var Century, Year, Month, Day, error_message, weekDay, male, female
+var Century, Year, Month, Day, error_message, weekDay, male, female, Buttons
 //inputData
 function inputData(){
     var Century = document.getElementById("century"). value;
@@ -14,7 +14,6 @@ function inputData(){
     if(Century <==0 || Century>20){
         text = "Please Enter Valid Century i.e 18,19 or 20";
         error_message.innerHTML = text;
-        return false;
     }
     if(Year.length != 2){
         text = "Please Enter Valid Year i.e 98,99 or 08";
@@ -36,21 +35,17 @@ function inputData(){
 //calculation of the data
 function calculateDay(){
     inputData();
-       weekDay = (((Century/4)-2*Century-1)+((5*Year/4))+((26*(Month +1)/10))+Day) % 7;
+       var weekDay = (((Century/4)-2*Century-1)+((5*Year/4))+((26*(Month +1)/10))+Day) % 7;
        console.log(weekDay);
-       return (Math.floor(weekDay));
+       
        if (weekDay < 0) {
            weekDay = weekDay * -1;
-          }
+       }
        else if (weekDay > 0){
        return weekDay;
           }
-function confirmDay(){
-   weekday = calculateDay();
-      checkGender();
-   console.log("Day confirmation function works")
-        }
-    }
+
+ }
 //arrays
 let weekDays = [ "sunday", "monday", "tuesday", "wednesday","thursday", "friday","saturday"];
 let maleNames = ["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi","Kwame"];
@@ -58,20 +53,20 @@ let femaleNames = ["Akosua","Adwoa", "Abenaa","Akua", "Yaa", "Afua","Ama"];
 
  //for the male and female buttons
  function checkGender(){
-    var gen = document.getElementsByName("gend");
+    var gen = document.getElementsById("gend");
     if(gen[0].checked == true){
-        var Gender = "male";
+        var gen = "male";
     }
     else if(gen[1].checked == true){
-        var Gender = "female";
+        var gen = "female";
     }
         else {
          console.log("pass");
 
     }
  }
-  switch(Gender){
-      case Gender = "male":
+  switch(gen){
+      case gen = "male":
           switch(weekday){
               case(0):
               document.getElementById("result").innerHTML = "You were born on a SUNDAY." + " " + "Your Akan name is " + maleNames[0];
@@ -99,7 +94,7 @@ let femaleNames = ["Akosua","Adwoa", "Abenaa","Akua", "Yaa", "Afua","Ama"];
              
       }
       break;
-      case Gender = "female":
+      case gen = "female":
         switch(weekday){
             case(0 || -0):
             document.getElementById("result").innerHTML = "You were born on a SUNDAY." + " " + "Your Akan name is " + femaleNames[0];
