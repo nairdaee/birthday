99104c1ce6eb0efadb86f900681441422617cfb8
Adrian Etenyi <etadriano2@gmail.com>
2020-02-20 23:39:21 +0300

    add method that prevents form submittion

--- scripts1.js
+++ scripts1.js
@@ -1,7 +1,8 @@
 //this is the main function
 function execute() {
     var errormessage = "";
- //these are the control flows of that determine when an error message is to pop up   
+
+//these are the control flows of that determine when an error message is to pop up   
       if(document.getElementById("birthyear").value == "") {
         errormessage += "Please enter your year of birth \n";
         document.getElementById("birthyear").style.borderColor = "red";
@@ -18,7 +19,8 @@ function execute() {
         alert(errormessage)
         return false;
        }
-  //These are defined variables for the input data which will actually determine the day  
+
+//These are defined variables for the input data which will actually determine the day  
     var year = document.getElementById("birthyear").value;
     var month = document.getElementById("birthmonth").value;
     var day = document.getElementById("birthday").value;
@@ -26,7 +28,9 @@ function execute() {
       d .setDate(parseInt(day)) //this object allows us to set the day of the month and so on
       d .setMonth(parseInt(month)) 
       d .setFullYear(parseInt(year))
-  //if else for the variables above
+ 
+ 
+//if else for the variables above
     if(year <=0){
         alert("Enter valid year i.e 2001,1998....");
     }
@@ -38,10 +42,13 @@ function execute() {
     }
 
     var birthday = d .getDay(0)
-    //these are the arrays for the actual Akan names
+   
+//these are the arrays for the actual Akan names
     var maleOnes = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
     var femaleOnes = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-    //these are the conditions for the male and female radio buttons
+    
+    
+//these are the conditions for the male and female radio buttons
       if (male. checked ==true){
         var name = ((maleOnes[birthday]) .toString())
         alert(name)
@@ -52,4 +59,8 @@ function execute() {
         alert("Please input valid information")
       }
  
+//attempt to remove 405 error
+     document.getElementById("burron").addEventListener("click" ,function(event){
+     event.preventDefault()
+     })
 }
