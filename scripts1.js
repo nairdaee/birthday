402965f2086becd89e2c46ebc4c5f8b85297bde5
Adrian Etenyi <etadriano2@gmail.com>
2020-02-20 22:29:11 +0300

    rectify spacing and indentation

--- scripts1.js
+++ scripts1.js
@@ -1,35 +1,35 @@
 function execute() {
     var errormessage = "";
     
-    if(document.getElementById("hname").value == "") {
+      if(document.getElementById("hname").value == "") {
         errormessage += "Please enter your year of birth \n";
         document.getElementById("hname").style.borderColor = "red";
-      }
-    if(document.getElementById("iname").value == "") {
+       }
+      if(document.getElementById("iname").value == "") {
         errormessage += "Please enter your month of birth \n";
         document.getElementById("iname").style.borderColor = "red";
-      }
-    if(document.getElementById("jname").value == "") {
+       }
+      if(document.getElementById("jname").value == "") {
         errormessage += "Please enter your day of birth \n";
         document.getElementById("jname").style.borderColor = "red";
-      }
-    if (errormessage != "") {
+       }
+      if (errormessage != "") {
         alert(errormessage)
         return false;
-      }
+       }
       
-      var year = document.getElementById("hname").value;
-      var month = document.getElementById("iname").value;
-      var day = document.getElementById("jname").value;
-      var d = new Date()
+    var year = document.getElementById("hname").value;
+    var month = document.getElementById("iname").value;
+    var day = document.getElementById("jname").value;
+    var d = new Date()
       d .setDate(parseInt(day))
       d .setMonth(parseInt(month)) 
       d .setFullYear(parseInt(year))
     
-      var akan = d .getDay(0)
+    var akan = d .getDay(0)
     
-      var akan1 = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-      var akan2 = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+    var akan1 = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    var akan2 = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
     
       if (male. checked ==true){
         var name = ((akan1[akan]) .toString())
@@ -38,6 +38,6 @@ function execute() {
         var name = ((akan2[akan]) .toString())
         alert(name)
       } else {
-        alert("Input Correct information")
+        alert("Please input valid information")
       }
  }
